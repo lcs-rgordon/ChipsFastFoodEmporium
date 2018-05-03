5f62509f10302bea2689ee680191ee9d05dbbba6
Russell Gordon <rgordon@lcs-mba2410.lcs.on.ca>
2018-05-03 16:43:34 -0400

    Solution that uses dictionaries instead of arrays

--- ChipsFastFoodEmporium/main.swift
+++ ChipsFastFoodEmporium/main.swift
@@ -1,11 +1,3 @@
-//
-//  main.swift
-//  ChipsFastFoodEmporium
-//
-//  Created by Gordon, Russell on 2018-04-04.
-//  Copyright © 2018 Gordon, Russell. All rights reserved.
-//
-
 import Foundation
 
 // INPUT
@@ -49,7 +41,8 @@ func getMealChoice(withPrompt: String) -> Int {
     return choice
 }
 
-// Get the burger choice
+// Get all the choices
+print("Welcome to Chip's Fast Food Emporium")
 let burgerChoice = getMealChoice(withPrompt: "Please enter a burger choice: ")
 let sideChoice = getMealChoice(withPrompt: "Please enter a side order choice: ")
 let drinkChoice = getMealChoice(withPrompt: "Please enter a drink choice: ")
@@ -57,38 +50,40 @@ let dessertChoice = getMealChoice(withPrompt: "Please enter a dessert choice: ")
 
 // PROCESS
 // Data structure to store calorie counts for burgers
-var burgerCalorieCounts = Array(repeating: 0, count: 5)
+var burgerCalorieCounts : [Int : Int] = [:]
 burgerCalorieCounts[1] = 461 // Cheeseburger
 burgerCalorieCounts[2] = 431 // Fishburger
 burgerCalorieCounts[3] = 420 // Veggieburger
 burgerCalorieCounts[4] = 0   // Nothingburger
 
 // Data structure to store calorie counts for side orders
-var sideOrderCalorieCounts = Array(repeating: 0, count: 5)
+var sideOrderCalorieCounts : [Int : Int] = [:]
 sideOrderCalorieCounts[1] = 100 // Fries
 sideOrderCalorieCounts[2] = 57  // Baked potato
 sideOrderCalorieCounts[3] = 70  // Chef salad
 sideOrderCalorieCounts[4] = 0   // Nothing for side order
 
 // Data structure to store calorie counts for drinks
-var drinkCaloriesCounts = Array(repeating: 0, count: 5)
+var drinkCaloriesCounts : [Int : Int] = [:]
 drinkCaloriesCounts[1] = 130 // Soft drink
 drinkCaloriesCounts[2] = 160 // Orange juice
 drinkCaloriesCounts[3] = 118 // Milk
 drinkCaloriesCounts[4] = 0   // Nothing for drink
 
 // Data structure to store calorie counts for desserts
-var dessertCalorieCounts = Array(repeating: 0, count: 5)
+var dessertCalorieCounts : [Int : Int] = [:]
 dessertCalorieCounts[1] = 167   // Apple pie
 dessertCalorieCounts[2] = 266   // Sundae
 dessertCalorieCounts[3] = 75    // Fruit cup
 dessertCalorieCounts[4] = 0     // Nothing for dessert
 
 // Calculate total calories
-let totalCalories = burgerCalorieCounts[burgerChoice] +
-                    sideOrderCalorieCounts[sideChoice] +
-                    drinkCaloriesCounts[drinkChoice] +
-                    dessertCalorieCounts[dessertChoice]
+// It's OK to force unwrap the values, because we know they exist
+// (we just added the values for all possible keys to the dictionaries)
+let totalCalories = burgerCalorieCounts[burgerChoice]! +
+                    sideOrderCalorieCounts[sideChoice]! +
+                    drinkCaloriesCounts[drinkChoice]! +
+                    dessertCalorieCounts[dessertChoice]!
 
 // OUTPUT
 // Report results to the user here
